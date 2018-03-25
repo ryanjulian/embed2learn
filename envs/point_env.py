57d1b3ed0b1a9e295c01124f903b85dc8b26704d
Ryan Julian <rjulian@usc.edu>
2018-03-25 13:59:23 -0700

    Fix PointEnv formatting

--- envs/point_env.py
+++ envs/point_env.py
@@ -11,19 +11,13 @@ from sandbox.rocky.tf.spaces.box import Box
 
 MAX_SHOWN_TRACES = 10
 
-TRACE_COLORS = [
-    (80, 150, 0),
-    (100, 180, 10),
-    (100, 210, 30),
-    (140, 230, 50),
-    (180, 250, 150)
-]
+TRACE_COLORS = [(80, 150, 0), (100, 180, 10), (100, 210, 30), (140, 230, 50),
+                (180, 250, 150)]
 BRIGHT_COLOR = (200, 200, 200)
 DARK_COLOR = (150, 150, 150)
 
 
 class PointEnv(Env, Serializable):
-
     def __init__(self, *args, goal=(0, 0), show_traces=True, **kwargs):
         super(PointEnv, self).__init__(*args, **kwargs)
 
@@ -75,39 +69,40 @@ class PointEnv(Env, Serializable):
             pygame.init()
             caption = "Point Environment"
             pygame.display.set_caption(caption)
-            self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+            self.screen = pygame.display.set_mode((self.screen_width,
+                                                   self.screen_height))
 
         self.screen.fill((255, 255, 255))
 
         # draw grid
         for x in range(25):
             dx = -6. + x * 0.5
-            pygame.draw.line(self.screen, DARK_COLOR if x % 2 == 0 else BRIGHT_COLOR,
-                             self._to_screen((dx, -10)), self._to_screen((dx, 10)))
+            pygame.draw.line(self.screen, DARK_COLOR
+                             if x % 2 == 0 else BRIGHT_COLOR,
+                             self._to_screen((dx, -10)),
+                             self._to_screen((dx, 10)))
         for y in range(25):
             dy = -6. + y * 0.5
-            pygame.draw.line(self.screen, DARK_COLOR if y % 2 == 0 else BRIGHT_COLOR,
-                             self._to_screen((-10, dy)), self._to_screen((10, dy)))
+            pygame.draw.line(self.screen, DARK_COLOR
+                             if y % 2 == 0 else BRIGHT_COLOR,
+                             self._to_screen((-10, dy)),
+                             self._to_screen((10, dy)))
 
         # draw point
-        pygame.draw.circle(
-            self.screen,
-            (40, 180, 10),
-            self._to_screen(self._point), 10, 0)
+        pygame.draw.circle(self.screen, (40, 180, 10),
+                           self._to_screen(self._point), 10, 0)
 
         # draw goal
-        pygame.draw.circle(
-            self.screen,
-            (255, 40, 0),
-            self._to_screen(self._goal), 10, 0)
+        pygame.draw.circle(self.screen, (255, 40, 0),
+                           self._to_screen(self._goal), 10, 0)
 
         # draw traces
         if self.show_traces:
             for i, trace in enumerate(self._traces):
                 if len(trace) > 1:
-                    pygame.draw.lines(self.screen,
-                                      TRACE_COLORS[-min(len(TRACE_COLORS)-1, i)],
-                                      False,
-                                      [self._to_screen(p) for p in trace])
+                    pygame.draw.lines(
+                        self.screen,
+                        TRACE_COLORS[-min(len(TRACE_COLORS) - 1, i)], False,
+                        [self._to_screen(p) for p in trace])
 
         pygame.display.flip()
