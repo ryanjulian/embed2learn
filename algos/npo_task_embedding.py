WIP fix task2action backprop

algos/npo_task_embedding.py
@@ -8,6 +8,7 @@ from rllab.misc import ext
 from rllab.misc import special
 from rllab.misc.overrides import overrides
 import rllab.misc.logger as logger
+from sandbox.embed2learn.embeddings.multitask_policy import MultitaskPolicy
 
 from sandbox.rocky.tf.algos.batch_polopt import BatchPolopt
 from sandbox.rocky.tf.misc import tensor_utils
@@ -44,17 +45,16 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
                  optimizer_args=None,
                  step_size=0.01,
                  policy_ent_coeff=1e-2,
-                 task_encoder=None,
+                 policy: MultitaskPolicy = None,
                  task_encoder_ent_coeff=1e-5,
                  trajectory_encoder=None,
                  trajectory_encoder_ent_coeff=1e-3,
                  **kwargs):
         Serializable.quick_init(self, locals())
         assert kwargs['env'].task_space
-        assert isinstance(task_encoder, Embedding)
+        assert isinstance(policy, MultitaskPolicy)
         assert isinstance(trajectory_encoder, Embedding)
 
-        self.task_encoder = task_encoder
         self.traj_encoder = trajectory_encoder
 
         # Joint optimizer for policy, task encoder and trajectory encoder
@@ -67,11 +67,10 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
 
         sampler_cls = TaskEmbeddingSampler
         sampler_args = dict(
-            task_encoder=self.task_encoder,
             trajectory_encoder=self.traj_encoder,
         )
         super(NPOTaskEmbedding, self).__init__(
-            sampler_cls=sampler_cls, sampler_args=sampler_args, **kwargs)
+            sampler_cls=sampler_cls, sampler_args=sampler_args, policy=policy, **kwargs)
 
     @overrides
     def init_opt(self):
@@ -99,7 +98,7 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
         if is_recurrent:
             raise NotImplementedError
 
-        latent_obs_space = concat_spaces(self.task_encoder.latent_space,
+        task_obs_space = concat_spaces(self.task_encoder.latent_space,
                                          self.env.observation_space)
 
         #### Policy and loss function ##########################################
@@ -176,11 +175,11 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
             # Calculate entropy terms
             # 1. Task encoder total entropy
             with tf.variable_scope('task_encoder_entropy'):
-                task_dim = self.task_encoder.input_space.flat_dim
+                task_dim = self.policy.embedding.input_space.flat_dim
                 all_task_one_hots = tf.one_hot(np.arange(task_dim), task_dim)
-                all_task_dists = self.task_encoder.dist_info_sym(
+                all_task_dists = self.policy.embedding.dist_info_sym(
                     all_task_one_hots)
-                all_task_entropies = self.task_encoder.entropy_sym(
+                all_task_entropies = self.policy.embedding.entropy_sym(
                     all_task_dists)
                 task_enc_entropy = tf.reduce_mean(all_task_entropies)
 

launchers/trpo_point_embed.py
@@ -4,6 +4,7 @@ from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
 from rllab.misc.instrument import run_experiment_lite
 from rllab.misc.ext import set_seed
 from rllab.envs.env_spec import EnvSpec
+from sandbox.embed2learn.embeddings.gaussian_mlp_multitask_policy import GaussianMLPMultitaskPolicy
 
 from sandbox.rocky.tf.policies.gaussian_mlp_policy import GaussianMLPPolicy
 from sandbox.rocky.tf.envs.base import TfEnv
@@ -29,11 +30,11 @@ TASK_ARGS = [TASKS[t]['args'] for t in TASK_NAMES]
 TASK_KWARGS = [TASKS[t]['kwargs'] for t in TASK_NAMES]
 
 # Embedding params
-LATENT_LENGTH = 8
+LATENT_LENGTH = 4
 TRAJ_ENC_WINDOW = 5
 
 
-def run_task(*_):
+def run_task(plot=False, *_):
     set_seed(0)
 
     # Environment
@@ -66,16 +67,8 @@ def run_task(*_):
 
     task_embed_spec = EmbeddingSpec(env.task_space, latent_space)
     traj_embed_spec = EmbeddingSpec(traj_space, latent_space)
-    latent_obs_space = concat_spaces(latent_space, env.observation_space)
-    env_spec_embed = EnvSpec(latent_obs_space, env.action_space)
-
-    # Base policy
-    policy = GaussianMLPPolicy(
-        name="policy",
-        env_spec=env_spec_embed,
-        hidden_sizes=(64, 32),
-        adaptive_std=True,  # Must be True for embedding learning
-    )
+    task_obs_space = concat_spaces(env.task_space, env.observation_space)
+    env_spec_embed = EnvSpec(task_obs_space, env.action_space)
 
     # Embeddings
     task_embedding = GaussianMLPEmbedding(
@@ -85,10 +78,20 @@ def run_task(*_):
         adaptive_std=True,  # Must be True for embedding learning
     )
 
+    # TODO(): rename to inference_network
     traj_embedding = GaussianMLPEmbedding(
         name="traj_embedding",
         embedding_spec=traj_embed_spec,
         hidden_sizes=(64, 64),
+        adaptive_std=True,  # Must be True for embedding learning
+    )
+
+    # Multitask policy
+    policy = GaussianMLPMultitaskPolicy(
+        name="policy",
+        env_spec=env_spec_embed,
+        embedding=task_embedding,
+        hidden_sizes=(64, 32),
         adaptive_std=True,  # Must be True for embedding learning
     )
 
@@ -98,21 +101,26 @@ def run_task(*_):
         env=env,
         policy=policy,
         baseline=baseline,
-        task_encoder=task_embedding,
         trajectory_encoder=traj_embedding,
         batch_size=4000,
         max_path_length=100,
         n_itr=1100,
         discount=0.99,
         step_size=0.01,
-        plot=False,
+        plot=plot,
+
+        # TODO comment out
+        policy_ent_coeff=0,
+        task_encoder_ent_coeff=0,
+        trajectory_encoder_ent_coeff=0,
     )
     algo.train()
 
 
-run_experiment_lite(
-    run_task,
-    exp_prefix='trpo_point_embed',
-    n_parallel=N_PARALLEL,
-    plot=False,
-)
+# run_experiment_lite(
+#     run_task,
+#     exp_prefix='trpo_point_embed',
+#     n_parallel=N_PARALLEL,
+#     plot=False,
+# )
+run_task(plot=True)