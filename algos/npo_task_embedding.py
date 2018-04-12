WIP fix task2action backprop

algos/npo_task_embedding.py
@@ -80,7 +80,7 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
 
         # Optimize policy, task_encoder and traj_encoder jointly
         targets = JointParameterized(
-            components=[self.policy, self.task_encoder, self.traj_encoder])
+            components=[self.policy, self.traj_encoder])
 
         # TODO(): should we consider KL constraints for all three networks?
         self.optimizer.update_opt(
@@ -99,13 +99,18 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
         if is_recurrent:
             raise NotImplementedError
 
-        task_obs_space = self.policy.observation_space
+        obs_space = self.policy.observation_space
+        task_space = self.policy.task_space
 
         #### Policy and loss function ##########################################
 
         # Input variables
-        obs_var = task_obs_space.new_tensor_variable(
+        obs_var = obs_space.new_tensor_variable(
             'obs',
+            extra_dims=1 + 1,
+        )
+        task_var = task_space.new_tensor_variable(
+            'task',
             extra_dims=1 + 1,
         )
         action_var = self.env.action_space.new_tensor_variable(
@@ -126,10 +131,10 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
             'trajectory',
             extra_dims=1 + 1,
         )
-        # latent_var = self.task_encoder.latent_space.new_tensor_variable(
-        #     'latent',
-        #     extra_dims=1 + 1,
-        # )
+        latent_var = self.policy.latent_space.new_tensor_variable(
+            'latent',
+            extra_dims=1 + 1,
+        )
         valid_var = tf.placeholder(
             tf.float32, shape=[None, None], name="valid")
 
@@ -160,16 +165,20 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
         # Flatten
         with tf.variable_scope('flatten'):
             obs_flat = flatten_batch(obs_var)
+            task_flat = flatten_batch(task_var)
             act_flat = flatten_batch(action_var)
             traj_flat = flatten_batch(trajectory_var)
-            latent_flat = flatten_batch(self.policy.latent_var)
+            # latent_flat = flatten_batch(self.policy.latent_var)
+            latent_flat = flatten_batch(latent_var)
             valid_flat = flatten_batch(valid_var)
             state_info_flat = flatten_batch_dict(state_info_vars)
             old_dist_info_flat = flatten_batch_dict(old_dist_info_vars)
 
         # Calculate policy distributions for each timestep
         # TODO: may need to freeze this for all three op steps
-        dist_info_vars = self.policy.dist_info_sym(obs_flat, state_info_flat)
+        # dist_info_vars = self.policy.dist_info_sym(obs_flat, state_info_flat)
+        latent_obs_flat = tf.concat((latent_flat, obs_flat), axis=1)
+        dist_info_vars = self.policy.dist_info_sym(latent_obs_flat, state_info_flat)
 
         with tf.variable_scope('entropies'):
             # Calculate entropy terms
@@ -411,7 +420,7 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
             baseline_var,
             trajectory_var,
             task_var,
-            # latent_var,
+            latent_var,
             valid_var,
         ] + state_info_vars_list + old_dist_info_vars_list \
           + task_enc_state_info_vars_list + task_enc_old_dist_info_vars_list \
@@ -425,7 +434,7 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
         self._baseline_var = baseline_var
         self._trajectory_var = trajectory_var
         self._task_var = task_var
-        # self._latent_var = latent_var
+        self._latent_var = latent_var
         self._valid_var = valid_var
         self._state_info_vars_list = state_info_vars_list
         self._old_dist_info_vars_list = old_dist_info_vars_list
@@ -457,63 +466,63 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
         self._traj_enc_mean_kl = traj_enc_mean_kl
 
         # DEBUG CPU VERSION ####################################################
-        with tf.variable_scope('cpu_debug'):
-            cpu_obs_var = latent_obs_space.new_tensor_variable(
-                'obs_cpu',
-                extra_dims=1 + is_recurrent,
-            )
-            cpu_action_var = self.env.action_space.new_tensor_variable(
-                'action_cpu',
-                extra_dims=1 + is_recurrent,
-            )
-            cpu_advantage_var = tensor_utils.new_tensor(
-                'advantage_cpu',
-                ndim=1 + is_recurrent,
-                dtype=tf.float32,
-            )
-            cpu_old_dist_info_vars = {
-                k: tf.placeholder(
-                    tf.float32,
-                    shape=[None] * (1 + is_recurrent) + list(shape),
-                    name='cpu_old_%s' % k)
-                for k, shape in dist.dist_info_specs
-            }
-            cpu_old_dist_info_vars_list = [
-                cpu_old_dist_info_vars[k] for k in dist.dist_info_keys
-            ]
-
-            cpu_state_info_vars = {
-                k: tf.placeholder(
-                    tf.float32,
-                    shape=[None] * (1 + is_recurrent) + list(shape),
-                    name='cpu_old_state_%s' % k)
-                for k, shape in self.policy.state_info_specs
-            }
-            cpu_state_info_vars_list = [
-                cpu_state_info_vars[k] for k in self.policy.state_info_keys
-            ]
-
-            cpu_dist_info_vars = self.policy.dist_info_sym(
-                cpu_obs_var, cpu_state_info_vars)
-            cpu_kl = dist.kl_sym(cpu_old_dist_info_vars, cpu_dist_info_vars)
-            cpu_lr = dist.likelihood_ratio_sym(
-                cpu_action_var, cpu_old_dist_info_vars, cpu_dist_info_vars)
-            cpu_mean_kl = tf.reduce_mean(cpu_kl)
-            cpu_surr_loss = -tf.reduce_mean(cpu_lr * cpu_advantage_var)
-
-        # Inputs
-        self._cpu_obs_var = cpu_obs_var
-        self._cpu_action_var = cpu_action_var
-        self._cpu_advantage_var = cpu_advantage_var
-        self._cpu_state_info_vars_list = cpu_state_info_vars_list
-        self._cpu_old_dist_info_vars_list = cpu_old_dist_info_vars_list
-
-        # Outputs
-        self._cpu_dist_info_vars = cpu_dist_info_vars
-        self._cpu_kl = cpu_kl
-        self._cpu_lr = cpu_lr
-        self._cpu_mean_kl = cpu_mean_kl
-        self._cpu_surr_loss = cpu_surr_loss
+        # with tf.variable_scope('cpu_debug'):
+        #     cpu_obs_var = latent_obs_space.new_tensor_variable(
+        #         'obs_cpu',
+        #         extra_dims=1 + is_recurrent,
+        #     )
+        #     cpu_action_var = self.env.action_space.new_tensor_variable(
+        #         'action_cpu',
+        #         extra_dims=1 + is_recurrent,
+        #     )
+        #     cpu_advantage_var = tensor_utils.new_tensor(
+        #         'advantage_cpu',
+        #         ndim=1 + is_recurrent,
+        #         dtype=tf.float32,
+        #     )
+        #     cpu_old_dist_info_vars = {
+        #         k: tf.placeholder(
+        #             tf.float32,
+        #             shape=[None] * (1 + is_recurrent) + list(shape),
+        #             name='cpu_old_%s' % k)
+        #         for k, shape in dist.dist_info_specs
+        #     }
+        #     cpu_old_dist_info_vars_list = [
+        #         cpu_old_dist_info_vars[k] for k in dist.dist_info_keys
+        #     ]
+        #
+        #     cpu_state_info_vars = {
+        #         k: tf.placeholder(
+        #             tf.float32,
+        #             shape=[None] * (1 + is_recurrent) + list(shape),
+        #             name='cpu_old_state_%s' % k)
+        #         for k, shape in self.policy.state_info_specs
+        #     }
+        #     cpu_state_info_vars_list = [
+        #         cpu_state_info_vars[k] for k in self.policy.state_info_keys
+        #     ]
+        #
+        #     cpu_dist_info_vars = self.policy.dist_info_sym(
+        #         cpu_obs_var, cpu_state_info_vars)
+        #     cpu_kl = dist.kl_sym(cpu_old_dist_info_vars, cpu_dist_info_vars)
+        #     cpu_lr = dist.likelihood_ratio_sym(
+        #         cpu_action_var, cpu_old_dist_info_vars, cpu_dist_info_vars)
+        #     cpu_mean_kl = tf.reduce_mean(cpu_kl)
+        #     cpu_surr_loss = -tf.reduce_mean(cpu_lr * cpu_advantage_var)
+        #
+        # # Inputs
+        # self._cpu_obs_var = cpu_obs_var
+        # self._cpu_action_var = cpu_action_var
+        # self._cpu_advantage_var = cpu_advantage_var
+        # self._cpu_state_info_vars_list = cpu_state_info_vars_list
+        # self._cpu_old_dist_info_vars_list = cpu_old_dist_info_vars_list
+        #
+        # # Outputs
+        # self._cpu_dist_info_vars = cpu_dist_info_vars
+        # self._cpu_kl = cpu_kl
+        # self._cpu_lr = cpu_lr
+        # self._cpu_mean_kl = cpu_mean_kl
+        # self._cpu_surr_loss = cpu_surr_loss
         #######################################################################
 
         # Functions
@@ -540,7 +549,7 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
         # Collect input values
         all_input_values = tuple(
             ext.extract(samples_data, 'observations', 'actions', 'rewards',
-                        'baselines', 'trajectories', 'tasks', # 'latents',
+                        'baselines', 'trajectories', 'tasks', 'latents',
                         'valids'))
         # add policy params
         agent_infos = samples_data["agent_infos"]
@@ -615,18 +624,18 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
             self._task_var: samples_data['tasks'],
             # self._latent_var: samples_data['latents'],
             self._valid_var: samples_data['valids'],
-            self._cpu_obs_var: samples_data['cpu_obs'],
-            self._cpu_action_var: samples_data['cpu_act'],
-            self._cpu_advantage_var: samples_data['cpu_adv'],
+            # self._cpu_obs_var: samples_data['cpu_obs'],
+            # self._cpu_action_var: samples_data['cpu_act'],
+            # self._cpu_advantage_var: samples_data['cpu_adv'],
         }
         for idx, v in enumerate(self._state_info_vars_list):
             feed[v] = state_info_list[idx]
         for idx, v in enumerate(self._old_dist_info_vars_list):
             feed[v] = dist_info_list[idx]
-        for idx, v in enumerate(self._cpu_state_info_vars_list):
-            feed[v] = cpu_state_info_list[idx]
-        for idx, v in enumerate(self._cpu_old_dist_info_vars_list):
-            feed[v] = cpu_dist_info_list[idx]
+        # for idx, v in enumerate(self._cpu_state_info_vars_list):
+        #     feed[v] = cpu_state_info_list[idx]
+        # for idx, v in enumerate(self._cpu_old_dist_info_vars_list):
+        #     feed[v] = cpu_dist_info_list[idx]
         # for idx, v in enumerate(self._task_enc_state_info_vars_list):
         #     feed[v] = task_enc_state_info_list[idx]
         # for idx, v in enumerate(self._task_enc_old_dist_info_vars_list):
@@ -666,21 +675,21 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
             'surr_loss': self._surr_loss,
         }
         cpu_steps = {
-            'dist_info_vars': self._cpu_dist_info_vars,
-            'kl': self._cpu_kl,
-            'lr': self._cpu_lr,
-            'mean_kl': self._cpu_mean_kl,
-            'surr_loss': self._cpu_surr_loss,
+            # 'dist_info_vars': self._cpu_dist_info_vars,
+            # 'kl': self._cpu_kl,
+            # 'lr': self._cpu_lr,
+            # 'mean_kl': self._cpu_mean_kl,
+            # 'surr_loss': self._cpu_surr_loss,
         }
         f_gpu, f_cpu = sess.run((gpu_steps, cpu_steps), feed_dict=feed)
 
         # Advantage step
         adv_tf = sess.run(self._f_adv, feed_dict=feed)
-        adv_cpu = samples_data['cpu_adv']
-        dadv = np.sqrt(np.sum((adv_cpu - adv_tf)**2))
+        # adv_cpu = samples_data['cpu_adv']
+        # dadv = np.sqrt(np.sum((adv_cpu - adv_tf)**2))
         #print('adv_tf: {}'.format(adv_tf))
         #print('adv_cpu: {}'.format(adv_cpu))
-        logger.record_tabular('dAdv', dadv)
+        # logger.record_tabular('dAdv', dadv)
         #print('mean(adv_cpu): {}'.format(np.mean(adv_cpu)))
         #print('mean(adv_tf): {}'.format(np.mean(adv_tf)))
         #print('std(adv_cpu): {}'.format(np.std(adv_cpu)))
@@ -730,12 +739,12 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
         # print('GPU KL: {}'.format(f_gpu['kl']))
 
         # Delta step
-        delta_tf = sess.run(self._f_deltas, feed_dict=feed)
-        delta_cpu = samples_data['cpu_deltas']
-        ddelta = np.sqrt(np.sum((delta_cpu - delta_tf)**2))
+        # delta_tf = sess.run(self._f_deltas, feed_dict=feed)
+        # delta_cpu = samples_data['cpu_deltas']
+        # ddelta = np.sqrt(np.sum((delta_cpu - delta_tf)**2))
         #print('deltas_tf: {}'.format(delta_tf))
         #print('deltas_cpu: {}'.format(delta_cpu))
-        logger.record_tabular('dDelta', ddelta)
+        # logger.record_tabular('dDelta', ddelta)
 
         # Baselines shift
         # base_shift = sess.run(self._f_base_shift, feed_dict=feed)

embeddings/gaussian_mlp_multitask_policy.py
@@ -22,6 +22,7 @@ class GaussianMLPMultitaskPolicy(StochasticMultitaskPolicy, LayersPowered, Seria
             name,
             env_spec,
             embedding: StochasticEmbedding,
+            task_space,
             hidden_sizes=(32, 32),
             learn_std=True,
             init_std=1.0,
@@ -58,29 +59,33 @@ class GaussianMLPMultitaskPolicy(StochasticMultitaskPolicy, LayersPowered, Seria
         Serializable.quick_init(self, locals())
         assert isinstance(env_spec.action_space, Box)
 
-        super(GaussianMLPMultitaskPolicy, self).__init__(env_spec, embedding)
+        super(GaussianMLPMultitaskPolicy, self).__init__(env_spec, embedding, task_space)
 
         with tf.variable_scope(name):
+            task_obs_dim = self.task_observation_space.flat_dim
+            action_dim = self.action_space.flat_dim
+            latent_dim = self.latent_space.flat_dim
+            obs_dim = self.observation_space.flat_dim
 
-            task_obs_dim = env_spec.observation_space.flat_dim
-            action_dim = env_spec.action_space.flat_dim
-            latent_dim = self._embedding.latent_space.flat_dim
-
-            # observations from vanilla environment
-            env_obs_dim = task_obs_dim - self._embedding.input_space.flat_dim
             # task embedding + plain obs
-            latent_obs_dim = latent_dim + env_obs_dim
+            latent_obs_dim = latent_dim + obs_dim
 
-            self.latent_var = tf.placeholder(tf.float32, (None, latent_dim), name='task_embedding')
-            self._env_obs_sym = tf.placeholder(tf.float32, (None, env_obs_dim), name='env_obs')
-            self._policy_input = tf.concat((self.latent_var, self._env_obs_sym), axis=1, name='policy_input')
+            self.onehot_input_var = self._embedding._mean_network.input_layer.input_var
+            self.env_input_var = tf.placeholder(tf.float32, (None, obs_dim), name='env_obs')
+
+            embed_dist_info_sym = self._embedding.dist_info_sym(
+                self._embedding._mean_network.input_layer.input_var, dict())
+            self.latent_var = embed_dist_info_sym["mean"]
+
+            # self.latent_var = self._embedding._l_mean.get_output_for(self.onehot_input_var)  # tf.placeholder(tf.float32, (None, latent_dim), name='task_embedding')
+            self._policy_input_var = tf.concat((self.latent_var, self.env_input_var), axis=1, name='policy_input')
 
             # create network
             if mean_network is None:
                 mean_network = MLP(
                     name="mean_network",
                     input_shape=(latent_obs_dim,),
-                    input_var=self._policy_input,
+                    input_var=self._policy_input_var,
                     output_dim=action_dim,
                     hidden_sizes=hidden_sizes,
                     hidden_nonlinearity=hidden_nonlinearity,
@@ -149,10 +154,20 @@ class GaussianMLPMultitaskPolicy(StochasticMultitaskPolicy, LayersPowered, Seria
             mean_var = dist_info_sym["mean"]
             log_std_var = dist_info_sym["log_std"]
 
-            self._f_dist = tensor_utils.compile_function(
-                inputs=[self._policy_input],
+            self._task_obs_action_dist = tensor_utils.compile_function(
+                inputs=[self.onehot_input_var, self.env_input_var],
+                outputs=[mean_var, log_std_var, self.latent_var],
+            )
+
+            self._latent_obs_action_dist = tensor_utils.compile_function(
+                inputs=[self.latent_var, self.env_input_var],
                 outputs=[mean_var, log_std_var],
             )
+
+            # self._f_dist = tensor_utils.compile_function(
+            #     inputs=[self.latent_var, self.env_input_var],
+            #     outputs=[mean_var, log_std_var],
+            # )
 
     @property
     def vectorized(self):
@@ -172,18 +187,36 @@ class GaussianMLPMultitaskPolicy(StochasticMultitaskPolicy, LayersPowered, Seria
 
     @overrides
     def get_action(self, observation):
+        """
+
+        :param observation: task onehot + env observation
+        :return: action, dict
+        """
+        flat_task_obs = self.task_observation_space.flatten(observation)
+        flat_task, flat_obs = self.split_observation(flat_task_obs)
+        # evaluate embedding
+        mean, log_std, latent = [x[0] for x in self._task_obs_action_dist([flat_task, flat_obs])]
+        rnd = np.random.normal(size=mean.shape)
+        action = rnd * np.exp(log_std) + mean
+        return action, dict(mean=mean, log_std=log_std, latent=latent)
+
+    def get_actions(self, observations):
+        # TODO implement split_observation_n(...)
+        flat_obs = self.task_observation_space.flatten_n(observations)
+        means, log_stds, latents = self._task_obs_action_dist(flat_obs)
+        rnd = np.random.normal(size=means.shape)
+        actions = rnd * np.exp(log_stds) + means
+        return actions, dict(mean=means, log_std=log_stds, latent=latents)
+
+    @overrides
+    def get_action_from_latent(self, observation, latent):
         flat_obs = self.observation_space.flatten(observation)
-        mean, log_std = [x[0] for x in self._f_dist([flat_obs])]
+        flat_latent = self.latent_space.flatten(latent)
+        # xs = self._latent_obs_action_dist(flat_latent, flat_obs)
+        mean, log_std = [x[0] for x in self._latent_obs_action_dist([flat_latent], [flat_obs])]
         rnd = np.random.normal(size=mean.shape)
         action = rnd * np.exp(log_std) + mean
         return action, dict(mean=mean, log_std=log_std)
-
-    def get_actions(self, observations):
-        flat_obs = self.observation_space.flatten_n(observations)
-        means, log_stds = self._f_dist(flat_obs)
-        rnd = np.random.normal(size=means.shape)
-        actions = rnd * np.exp(log_stds) + means
-        return actions, dict(mean=means, log_std=log_stds)
 
     def get_reparam_action_sym(self, obs_var, action_var, old_dist_info_vars):
         """

embeddings/multitask_policy.py
@@ -1,13 +1,16 @@
 from sandbox.embed2learn.embeddings.base import Embedding, StochasticEmbedding
+from sandbox.embed2learn.embeddings.utils import concat_spaces
 from sandbox.rocky.tf.core.parameterized import Parameterized
 from sandbox.rocky.tf.policies.base import Policy, StochasticPolicy
 
 
 class MultitaskPolicy(Policy, Parameterized):
-    def __init__(self, env_spec, embedding: Embedding):
+    def __init__(self, env_spec, embedding: Embedding, task_space):
         Parameterized.__init__(self)
         self._env_spec = env_spec
         self._embedding = embedding
+        self._task_space = task_space
+        self._task_observation_space = concat_spaces(self._task_space, self._env_spec.observation_space)
 
     # Should be implemented by all policies
 
@@ -24,7 +27,7 @@ class MultitaskPolicy(Policy, Parameterized):
         raise NotImplementedError
 
     def get_latent(self, onehot):
-        raise NotImplementedError
+        return self._embedding.get_latent(onehot)
 
     def reset(self, dones=None):
         pass
@@ -50,8 +53,16 @@ class MultitaskPolicy(Policy, Parameterized):
         return self._embedding.embedding_spec
 
     @property
+    def task_space(self):
+        return self._task_space
+
+    @property
     def observation_space(self):
         return self._env_spec.observation_space
+
+    @property
+    def task_observation_space(self):
+        return self._task_observation_space
 
     @property
     def action_space(self):
@@ -97,11 +108,19 @@ class MultitaskPolicy(Policy, Parameterized):
         """
         pass
 
+    def split_observation(self, observation):
+        """
+        Splits up observation into task onehot and vanilla environment observation.
+        :param observation: task onehot concatenated with vanilla environment observation
+        :return: tuple (task onehot, vanilla environment observation)
+        """
+        return observation[self.task_space.flat_dim:], observation[:self.task_space.flat_dim]
+
 
 class StochasticMultitaskPolicy(StochasticPolicy, MultitaskPolicy):
 
-    def __init__(self, env_spec, embedding: StochasticEmbedding):
-        super().__init__(env_spec, embedding)
+    def __init__(self, env_spec, embedding: StochasticEmbedding, task_space):
+        super().__init__(env_spec, embedding, task_space)
         self._embedding = embedding
 
     @property

envs/multi_task_env.py
@@ -37,8 +37,8 @@ class MultiTaskEnv(Env):
 
     def step(self, action):
         obs, reward, done, info = self._active_env.step(action)
-        info['task'] = self.active_task_one_hot
-        obs = np.concatenate((info['task'], obs))
+        # info['task'] = self.active_task_one_hot
+        # obs = np.concatenate((info['task'], obs))
         return Step(obs, reward, done, **info)
 
     def render(self, *args, **kwargs):