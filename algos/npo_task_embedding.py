WIP fix task2action backprop

algos/npo_task_embedding.py
@@ -80,7 +80,7 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
 
         # Optimize policy, task_encoder and traj_encoder jointly
         targets = JointParameterized(
-            components=[self.policy, self.traj_encoder])
+            components=[self.policy, self.policy._embedding, self.traj_encoder])
 
         # TODO(): should we consider KL constraints for all three networks?
         self.optimizer.update_opt(
@@ -105,10 +105,10 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
         #### Policy and loss function ##########################################
 
         # Input variables
-        obs_var = obs_space.new_tensor_variable(
-            'obs',
-            extra_dims=1 + 1,
-        )
+        # obs_var = obs_space.new_tensor_variable(
+        #     'obs',
+        #     extra_dims=1 + 1,
+        # )
         # task_var = task_space.new_tensor_variable(
         #     'task',
         #     extra_dims=1 + 1,
@@ -164,11 +164,11 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
         # TODO: verify this works for tensors in general
         # Flatten
         with tf.variable_scope('flatten'):
-            obs_flat = flatten_batch(obs_var)
+            obs_flat = self.policy.env_input_var  # flatten_batch(self.policy.env_input_var)
             # task_flat = flatten_batch(task_var)
             act_flat = flatten_batch(action_var)
             traj_flat = flatten_batch(trajectory_var)
-            latent_flat = flatten_batch(self.policy.latent_var)
+            latent_flat = self.policy.latent_var
             # latent_flat = flatten_batch(latent_var)
             valid_flat = flatten_batch(valid_var)
             state_info_flat = flatten_batch_dict(state_info_vars)
@@ -178,7 +178,11 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
         # TODO: may need to freeze this for all three op steps
         # dist_info_vars = self.policy.dist_info_sym(obs_flat, state_info_flat)
         latent_obs_flat = tf.concat((self.policy.latent_var, obs_flat), axis=1)
-        dist_info_vars = self.policy.dist_info_sym(latent_obs_flat, state_info_flat)
+        # dist_info_vars = self.policy.dist_info_sym(latent_obs_flat, state_info_flat)
+        dist_info_vars = self.policy.dist_info_sym({
+            self.policy.env_input.input_var: obs_flat,
+            self.policy._embedding._mean_network.input_layer.input_var: self.policy.latent_var
+        }, state_info_flat)
 
         with tf.variable_scope('entropies'):
             # Calculate entropy terms
@@ -415,7 +419,7 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
         #### Input list ########################################################
         input_list = [
             self.policy.onehot_input_var,  # TODO find better way to provide task onehot
-            obs_var,
+            self.policy.env_input_var,
             action_var,
             reward_var,
             baseline_var,
@@ -430,7 +434,7 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
 
         #### DEBUG #############################################################
         # Inputs
-        self._obs_var = obs_var
+        self._obs_var = self.policy.env_input_var
         self._action_var = action_var
         self._reward_var = reward_var
         self._baseline_var = baseline_var
@@ -550,9 +554,10 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
     def optimize_policy(self, itr, samples_data):
         # Collect input values
         tasks = np.reshape(samples_data["tasks"], (-1, self.policy.task_space.flat_dim))
-        all_input_values = tasks,
+        obs = np.reshape(samples_data["observations"], (-1, self.policy.observation_space.flat_dim))
+        all_input_values = (tasks, obs)
         all_input_values += tuple(
-            ext.extract(samples_data, 'observations', 'actions', 'rewards',
+            ext.extract(samples_data, 'actions', 'rewards',
                         'baselines', 'trajectories', 'tasks', #'latents',
                         'valids'))
         # add policy params
@@ -624,17 +629,18 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
             cpu_agent_infos[k] for k in self.policy.distribution.dist_info_keys
         ]
         feed = {
-            self._obs_var: samples_data['observations'],
+            self._obs_var: obs,
             self._action_var: samples_data['actions'],
             self._reward_var: samples_data['rewards'],
             self._baseline_var: samples_data['baselines'],
             self._trajectory_var: samples_data['trajectories'],
-            self._task_var: samples_data['tasks'],
+            # self._task_var: samples_data['tasks'],
             # self._latent_var: samples_data['latents'],
             self._valid_var: samples_data['valids'],
             # self._cpu_obs_var: samples_data['cpu_obs'],
             # self._cpu_action_var: samples_data['cpu_act'],
             # self._cpu_advantage_var: samples_data['cpu_adv'],
+            self.policy.onehot_input_var: tasks
         }
         for idx, v in enumerate(self._state_info_vars_list):
             feed[v] = state_info_list[idx]
@@ -729,8 +735,8 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
         # surr_loss
         # print('CPU surr_loss: {}'.format(f_cpu['surr_loss']))
         # print('GPU surr_loss: {}'.format(f_gpu['surr_loss']))
-        dsurr_loss = f_cpu['surr_loss'] - f_gpu['surr_loss']
-        logger.record_tabular('dSurr_loss', dsurr_loss)
+        # dsurr_loss = f_cpu['surr_loss'] - f_gpu['surr_loss']
+        # logger.record_tabular('dSurr_loss', dsurr_loss)
 
         # mean_kl
         # print('CPU mean_kl: {}'.format(f_cpu['mean_kl']))
@@ -829,8 +835,8 @@ class NPOTaskEmbedding(BatchPolopt, Serializable):
 
         # Compute after-optimization diagnostics
         f_gpu, f_cpu = sess.run((gpu_steps, cpu_steps), feed_dict=feed)
-        latent_mean_kl = f_gpu['task_enc_mean_kl']
-        logger.record_tabular('TaskEncoder/MeanKL', latent_mean_kl)
+        # latent_mean_kl = f_gpu['task_enc_mean_kl']
+        # logger.record_tabular('TaskEncoder/MeanKL', latent_mean_kl)
 
         return dict()
 

embeddings/gaussian_mlp_multitask_policy.py
@@ -1,6 +1,9 @@
 import numpy as np
 import tensorflow as tf
 
+import itertools
+
+
 from sandbox.embed2learn.embeddings.base import StochasticEmbedding
 from sandbox.rocky.tf.core.layers_powered import LayersPowered
 import sandbox.rocky.tf.core.layers as L
@@ -70,23 +73,31 @@ class GaussianMLPMultitaskPolicy(StochasticMultitaskPolicy, LayersPowered, Seria
             # task embedding + plain obs
             latent_obs_dim = latent_dim + obs_dim
 
-            self.onehot_input_var = self._embedding._mean_network.input_layer.input_var
-            self.env_input_var = tf.placeholder(tf.float32, (None, obs_dim), name='env_obs')
+            self.task_input = self._embedding._mean_network.input_layer
+            self.onehot_input_var = self.task_input.input_var
+            # self.env_input_var = tf.placeholder(tf.float32, (None, obs_dim), name='env_obs')
+            self.env_input = L.InputLayer((None, obs_dim), name="policy_env_input")
+            self.env_input_var = self.env_input.input_var
 
             embed_dist_info_sym = self._embedding.dist_info_sym(
-                self._embedding._mean_network.input_layer.input_var, dict())
+                self.task_input.input_var, dict())
             self.latent_var = embed_dist_info_sym["mean"]
+            self.latent = L.InputLayer((None, latent_dim), self.latent_var, name="latent_input")
+            # self.latent_var = self._embedding._mean_network._l_out.get_output_for(self._embedding._mean_network._l_out.input_layer)
+            # self.latent_var = self._embedding._l_mean
             # self.latent_var = tf.reshape(self.latent_var, (None,) + tuple(self.latent_var.shape), name='latent')
 
-            # self.latent_var = self._embedding._l_mean.get_output_for(self.onehot_input_var)  # tf.placeholder(tf.float32, (None, latent_dim), name='task_embedding')
-            self._policy_input_var = tf.concat((self.latent_var, self.env_input_var), axis=1, name='policy_input')
+            # self.latent_var = tf.placeholder(tf.float32, (None, latent_dim), name='task_embedding')
+            # self._policy_input_var = tf.concat((self.latent_var, self.env_input_var), axis=1, name='policy_input')
+            self._policy_input = L.ConcatLayer((self.latent, self.env_input))
 
             # create network
             if mean_network is None:
                 mean_network = MLP(
                     name="mean_network",
                     input_shape=(latent_obs_dim,),
-                    input_var=self._policy_input_var,
+                    # input_var=self._policy_input_var,
+                    input_layer=self._policy_input,
                     output_dim=action_dim,
                     hidden_sizes=hidden_sizes,
                     hidden_nonlinearity=hidden_nonlinearity,
@@ -95,7 +106,7 @@ class GaussianMLPMultitaskPolicy(StochasticMultitaskPolicy, LayersPowered, Seria
             self._mean_network = mean_network
 
             l_mean = mean_network.output_layer
-            obs_var = mean_network.input_layer.input_var
+            # obs_var = mean_network.input_layer.input_var
 
             if std_network is not None:
                 l_std_param = std_network.output_layer
@@ -149,19 +160,25 @@ class GaussianMLPMultitaskPolicy(StochasticMultitaskPolicy, LayersPowered, Seria
 
             self._dist = DiagonalGaussian(action_dim)
 
-            LayersPowered.__init__(self, [l_mean, l_std_param])
+            LayersPowered.__init__(self,
+                                   [l_mean, l_std_param],
+                                   # [self.task_input, self.env_input]
+                                   )
 
-            dist_info_sym = self.dist_info_sym(mean_network.input_layer.input_var, dict())
+            dist_info_sym = self.dist_info_sym({
+                self.env_input.input_var: self.env_input.input_var,
+                self.task_input.input_var: self.task_input.input_var
+            }, dict())
             mean_var = dist_info_sym["mean"]
             log_std_var = dist_info_sym["log_std"]
 
             self._task_obs_action_dist = tensor_utils.compile_function(
-                inputs=[self.onehot_input_var, self.env_input_var],
+                inputs=[self.onehot_input_var, self.env_input.input_var],
                 outputs=[mean_var, log_std_var, self.latent_var],
             )
 
             self._latent_obs_action_dist = tensor_utils.compile_function(
-                inputs=[self.latent_var, self.env_input_var],
+                inputs=[self.latent_var, self.env_input.input_var],
                 outputs=[mean_var, log_std_var],
             )
 
@@ -173,6 +190,14 @@ class GaussianMLPMultitaskPolicy(StochasticMultitaskPolicy, LayersPowered, Seria
     @property
     def vectorized(self):
         return True
+
+    @overrides
+    def get_params_internal(self, **tags):
+        # TODO add
+        layers = L.get_all_layers(self._output_layers, treat_as_input=self._input_layers)
+        layers += L.get_all_layers(self._embedding._output_layers, treat_as_input=self._embedding._input_layers)
+        params = itertools.chain.from_iterable(l.get_params(**tags) for l in layers)
+        return L.unique(params)
 
     def dist_info_sym(self, obs_var, state_info_vars=None):
         mean_var, std_param_var = L.get_output([self._l_mean, self._l_std_param], obs_var)