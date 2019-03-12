9c47758214dacccefed51946d08ed9f027588cbb
Ryan Julian <rjulian@usc.edu>
2019-03-12 13:41:07 -0700

    Remove PR2 envs and associated launchers

--- embed2learn/envs/mujoco/__init__.py
+++ embed2learn/envs/mujoco/__init__.py
@@ -1,4 +1,3 @@
-from embed2learn.envs.mujoco.pr2_arm_clock_env import PR2ArmClockEnv
-from embed2learn.envs.mujoco.pr2_arm_env import PR2ArmEnv
+from embed2learn.envs.sequence_reacher_env import SequenceReacherEnv
 
-__all__ = ['PR2ArmClockEnv', 'PR2ArmEnv']
+__all__ = ['SequenceReacherEnv']
